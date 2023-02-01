a7a69ad5813ce755b516f52e3430ddb4a33ed170
yakup <lvashy86@gmail.com>
2023-02-01 17:23:56 +0300

    02

---
+++ largest3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+void find3Largest(int arr[], int n){
+  int first;
+  int second;
+  int third;
+
+  third = second = first = INT_MIN;
+
+  for(int i = 0; i < n; i++){
+    if(arr[i] > first) {
+      third = second;
+      second = first;
+      first = arr[i];
+    } else if(arr[i] > second){
+      third = second;
+      second = first;
+    } else if(arr[i] > third){
+      arr[i] = third;
+    }
+  }
+  std::cout<<"first: " << first << " second: " << second << " third: " << third;
+
+}
+
+
+int main() {
+  int arr[] = {1,2,3,4,5};
+
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  find3Largest(arr, n);
+
+  return 0;
+}
